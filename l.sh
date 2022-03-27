786fa0acdca56dba0140fb0d98650a33fe6f41f0
gitmining001 <101994494+gitmining001@users.noreply.github.com>
2022-03-28 03:22:21 +0700

    Update and rename n.sh to l.sh

---
+++ l.sh
@@ -0,0 +1,3 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+./ngrok http 8787 
--- n.sh
+++
@@ -1,4 +0,0 @@
-sudo apt install nodejs npm nodejs-legacy
-sudo npm install -g localtunnel
-lt --port 8787 
-
