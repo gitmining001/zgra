80eb3880416015cce127439953a7e4882416581b
gitmining001 <101994494+gitmining001@users.noreply.github.com>
2022-03-28 01:56:17 +0700

    Update rf.sh

--- rf.sh
+++ rf.sh
@@ -1,7 +1,2 @@
-adduser rstudio
-apt-get install gdebi-core
-wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2022.02.0-443-amd64.deb
-apt-get install gdebi-core
-gdebi -n rstudio-server-2022.02.0-443-amd64.deb
 apt-get install -g localtunnel
 lt --port 8787 
